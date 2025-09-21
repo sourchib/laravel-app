cf38be2c6cd453a32e6cc6ac80010a4c02a8ee80
sourchib <muchammadmuchib@gmail.com>
2025-09-22 02:53:14 +0700

    Add Unit tests folder with ExampleTest

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_basic_math()
+    {
+        $this->assertTrue(2 + 2 === 4);
+    }
+}
