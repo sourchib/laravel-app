b61ee10cfc02d8cbc1a9597e4a8652ebb733cc38
Muchammad Muchib Zainul Fikry <60887634+sourchib@users.noreply.github.com>
2025-09-22 07:00:14 +0700

    Update ArticleTest.php

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -1,10 +1,4 @@
-<?php
-
-namespace Tests\Feature;
-
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
-use App\Models\Article;
 
 class ArticleTest extends TestCase
 {
@@ -13,16 +7,16 @@ class ArticleTest extends TestCase
     /** @test */
     public function show_article()
     {
-        // Arrange: buat dummy article di database testing
         $article = Article::factory()->create([
             'title' => 'Test Judul',
-            'content' => 'Isi artikel untuk testing',
+            'slug' => 'test-judul',
+            'description' => 'Isi artikel untuk testing',
+            'body' => 'Body artikel',
+            'content' => 'Isi artikel lengkap untuk testing',
         ]);
 
-        // Act: request ke endpoint show article
-        $response = $this->get("/articles/{$article->id}");
+        $response = $this->get(route('articles.show', $article->id));
 
-        // Assert: cek status OK & data muncul
         $response->assertStatus(200);
         $response->assertSee($article->title);
     }
